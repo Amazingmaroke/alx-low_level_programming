f486bf90937746f553c0868e82a0b0aa87839854
chimaroke <marokemaroke2020@gmail.com>
2022-10-24 12:20:07 -0700

    8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,34 +1,22 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * puts_half - prints half of a string
- * if odd lenn, n = (length_of_the_string - 1) / 2
- * @str: string input
- * Return: half of input
+ * print_array - prints the items in an array
+ * @arr: array to be printed
+ * @n: number of element to print
+ * Return: 0
  */
-void puts_half(char *str)
+
+void print_array(int *arr, int n)
 {
-	int i, n, len;
+	int i;
 
-	len = 0;
-
-	/*for loop to count string length*/
-	for (i = 0; str[i] != '\0'; i++)
-		len++;
-
-	/*initialise n, then use n in for loop for even len*/
-	n = (len / 2);
-
-	/*Check if len is odd and divide by len by 2*/
-	if ((len % 2) == 1)
-
-		/*Check if len is odd and divide by len by 2*/
-		if ((len % 2) == 1)
-			n = ((len + 1) / 2);
-
-	/*for loop for even len*/
-	for (i = n; str[i] != '\0'; i++)
-		_putchar(str[i]);
-
-	_putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", arr[i]);
+		if (i < n - 1)
+			printf(", ");
+	}
+	putchar('\n');
 }
